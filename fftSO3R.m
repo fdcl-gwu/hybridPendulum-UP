e0e6fb4354b4430ab64f8b1f60999e68a9c2a55e
weixinwang <weixinwang442@gmail.com>
2020-10-07 22:50:59 -0400

    use 2B-1 grid for fftSO3 and ifftSO3

--- fftSO3R.m
+++ fftSO3R.m
@@ -20,13 +20,13 @@ if ~exist('func','var') || isempty(func)
     func = @(R,x) 1/(c*sqrt((2*pi)^3*det(SigmaC)))*...
         permute(exp(sum(sum(S.'.*R,1),2)),[3,2,1]).*...
         permute(exp(-0.5*sum(sum(permute(x-Miu-P*vR(R),[1,3,2]).*...
-        permute(x-Miu-P*vR(R),[1,3,2]).*SigmaC^-1,1),2)),[3,2,1]);
+            permute(x-Miu-P*vR(R),[1,3,2]).*SigmaC^-1,1),2)),[3,2,1]);
 end
 
 % grid over SO(3)
-alpha = reshape(pi/B*(0:(2*B-1)),1,1,[]);
+alpha = reshape(2*pi/(2*B-1)*(0:(2*B-2)),1,1,[]);
 beta = reshape(pi/(4*B)*(2*(0:(2*B-1))+1),1,1,[]);
-gamma = reshape(pi/B*(0:(2*B-1)),1,1,[]);
+gamma = reshape(2*pi/(2*B-1)*(0:(2*B-2)),1,1,[]);
 
 ca = cos(alpha);
 sa = sin(alpha);
@@ -35,14 +35,14 @@ sb = sin(beta);
 cg = cos(gamma);
 sg = sin(gamma);
 
-Ra = [ca,-sa,zeros(1,1,2*B);sa,ca,zeros(1,1,2*B);zeros(1,1,2*B),zeros(1,1,2*B),ones(1,1,2*B)];
+Ra = [ca,-sa,zeros(1,1,2*B-1);sa,ca,zeros(1,1,2*B-1);zeros(1,1,2*B-1),zeros(1,1,2*B-1),ones(1,1,2*B-1)];
 Rb = [cb,zeros(1,1,2*B),sb;zeros(1,1,2*B),ones(1,1,2*B),zeros(1,1,2*B);-sb,zeros(1,1,2*B),cb];
-Rg = [cg,-sg,zeros(1,1,2*B);sg,cg,zeros(1,1,2*B);zeros(1,1,2*B),zeros(1,1,2*B),ones(1,1,2*B)];
+Rg = [cg,-sg,zeros(1,1,2*B-1);sg,cg,zeros(1,1,2*B-1);zeros(1,1,2*B-1),zeros(1,1,2*B-1),ones(1,1,2*B-1)];
 
-R = zeros(3,3,2*B,2*B,2*B);
-for i = 1:2*B
+R = zeros(3,3,2*B-1,2*B,2*B-1);
+for i = 1:2*B-1
     for j = 1:2*B
-        for k = 1:2*B
+        for k = 1:2*B-1
             R(:,:,i,j,k) = Ra(:,:,i)*Rb(:,:,j)*Rg(:,:,k);
         end
     end
@@ -53,7 +53,7 @@ x = zeros(3,2*B,2*B,2*B);
 for i = 1:2*B
     for j = 1:2*B
         for k = 1:2*B
-            x(:,i,j,k) = [-1+2/20*(i-1);-1+2/20*(j-1);-1+2/20*(k-1)];
+            x(:,i,j,k) = [-1+2/(2*B)*(i-1);-1+2/(2*B)*(j-1);-1+2/(2*B)*(k-1)];
         end
     end
 end
@@ -61,24 +61,23 @@ end
 % weights
 w = zeros(1,2*B);
 for j = 1:2*B
-    w(j) = 1/(4*B^3)*sin(beta(j))*sum(1./(2*(0:B-1)+1).*sin((2*(0:B-1)+1)*beta(j)));
+    w(j) = 1/(B*(2*B-1)^2)*sin(beta(j))*sum(1./(2*(0:B-1)+1).*sin((2*(0:B-1)+1)*beta(j)));
 end
 
 % Wigner_d
 lmax = B-1;
-
 d = zeros(2*lmax+1,2*lmax+1,lmax+1,2*B);
 for j = 1:2*B
     d(:,:,:,j) = Wigner_d(beta(j),lmax);
 end
 
 % function values
-R_linInd = reshape(R,3,3,(2*B)^3);
-f = zeros(2*B,2*B,2*B,2*B,2*B,2*B);
-for i = 1:2*B
+R_linInd = reshape(R,3,3,(2*B)*(2*B-1)^2);
+f = zeros(2*B-1,2*B,2*B-1,2*B,2*B,2*B);
+for k = 1:2*B
     for j = 1:2*B
-        for k = 1:2*B
-            f(:,:,:,i,j,k) = reshape(func(R_linInd,x(:,i,j,k)),2*B,2*B,2*B);
+        parfor i = 1:2*B
+            f(:,:,:,i,j,k) = reshape(func(R_linInd,x(:,i,j,k)),2*B-1,2*B,2*B-1);
         end
     end
 end
@@ -86,7 +85,7 @@ end
 % fft
 F = zeros(2*lmax+1,2*lmax+1,lmax+1,2*B,2*B,2*B);
 
-F1 = zeros(2*B,2*B,2*B,2*B,2*B,2*B);
+F1 = zeros(2*B-1,2*B,2*B-1,2*B,2*B,2*B);
 for k = 1:2*B
     F1(:,k,:,:,:,:) = fftn(f(:,k,:,:,:,:));
 end
@@ -97,7 +96,7 @@ for l = 0:lmax
     for m = -l:l
         for n = -l:l
             F(m+lmax+1,n+lmax+1,l+1,:,:,:) = sum(w.*F1(m+lmax+1,:,n+lmax+1,:,:,:).*...
-                reshape(d(m+lmax+1,n+lmax+1,l+1,:),1,[]),2);
+                permute(d(m+lmax+1,n+lmax+1,l+1,:),[1,4,3,2]),2);
         end
     end
 end
--- ifftSO3R.m
+++ ifftSO3R.m
@@ -4,9 +4,7 @@ B = size(F,3);
 lmax = B-1;
 
 % grid over SO(3)
-alpha = pi/B*(0:(2*B-1));
 beta = pi/(4*B)*(2*(0:(2*B-1))+1);
-gamma = pi/B*(0:(2*B-1));
 
 % Wigner_d
 d = zeros(2*lmax+1,2*lmax+1,lmax+1,2*B);
@@ -15,7 +13,7 @@ for j = 1:2*B
 end
 
 % ifft
-F2 = zeros(2*B-1,2*B,2*B-1,2*B,2*B,2*B);
+F1 = zeros(2*B-1,2*B,2*B-1,2*B,2*B,2*B);
 for m = -lmax:lmax
     for n = -lmax:lmax
         lmin = max(abs(m),abs(n));
@@ -23,29 +21,17 @@ for m = -lmax:lmax
         
         for k = 1:2*B
             d_jk_betak = d(m+lmax+1,n+lmax+1,lmin+1:lmax+1,k);
-            F2(m+lmax+1,k,n+lmax+1,:,:,:) = sum((2*permute(lmin:lmax,...
+            F1(m+lmax+1,k,n+lmax+1,:,:,:) = sum((2*permute(lmin:lmax,...
                 [1,3,2])+1).*F_mn.*d_jk_betak,3);
         end
     end
 end
 
-F1 = zeros(2*B,2*B,2*B,2*B,2*B,2*B);
-for j1 = 1:2*B
-    for j2 = 1:2*B
-        for k = 1:2*B
-            F1(j1,k,j2,:,:,:) = sum(sum(permute(exp(-1i*(-B+1:B-1)*alpha(j1)),[2,1]).*...
-                permute(exp(-1i*(-B+1:B-1)*gamma(j2)),[1,3,2]).*F2(:,k,:,:,:,:),1),3);
-        end
-    end
-end
-
-f = zeros(2*B,2*B,2*B,2*B,2*B,2*B);
-for j1 = 1:2*B
-    for j2 = 1:2*B
-        for k = 1:2*B
-            f(j1,k,j2,:,:,:) = ifftn(F1(j1,k,j2,:,:,:));
-        end
-    end
+f = zeros(2*B-1,2*B,2*B-1,2*B,2*B,2*B);
+F1 = ifftshift(ifftshift(F1,1),3);
+F1 = flip(flip(F1,1),3);
+for k = 1:2*B
+    f(:,k,:,:,:,:) = ifftn(F1(:,k,:,:,:,:))*(2*B-1)^2;
 end
 
 end
