fd2a5e4f1ebba50db1e92085eb53fd8e9648f1ff
weixinwang <weixinwang442@gmail.com>
2021-07-28 15:05:08 -0400

    plotting

--- propagation/plotting/plotc.m
+++ propagation/plotting/plotc.m
@@ -24,21 +24,35 @@ end
 
 % plot
 for nt = 1:Nt
-    c = load(strcat(path,'/c',num2str(nt)));
-    c = c.c;
+    load(strcat(path,'/c',num2str(nt)),'c');
     
     f = figure;
-    surf(s1,s2,s3,c,'LineStyle','none','FaceColor','interp');
+    hold on;
+    h_surf = surf(s1,s2,s3,c,'LineStyle','none','FaceColor','interp',...
+        'FaceAlpha',0.8,'FaceLighting','gouraud');
+    myarrow([0,0,0],[1.2,0,0],'k',1,0.1,0.1*tand(15));
+    myarrow([0,0,0],[0,-1.2,0],'k',1,0.1,0.1*tand(15));
+    myarrow([0,0,0],[0,0,-1.2],'k',1,0.1,0.1*tand(15));
+    
+    h_light=light;
+    material dull;
+    h_light.Style='infinite';
+    h_light.Position=[-1 0 1]*10;
+    
+    set(h_surf,'FaceLighting','gouraud','AmbientStrength',0.95,...
+        'DiffuseStrength',0.05,'SpecularStrength',0,'BackFaceLighting','unlit');
 
-    xlim([-1,1]);
-    ylim([-1,1]);
-    zlim([-1,1]);
-    view([1,-1,0]);
+    xlim([-1.2,1.2]);
+    ylim([-1.2,1.2]);
+    zlim([-1.2,1.2]);
+    view([-0.1,1,-0.3]);
     axis equal;
+    axis off;
 
-    annotation('textbox','String',strcat('time: ',num2str((nt-1)/sf),' s'),'Position',[0.15,0.75,0.16,0.07]);
+    annotation('textbox','String',strcat('time: ',num2str((nt-1)/sf),' s'),...
+        'Position',[0.15,0.75,0.4,0.07],'LineStyle','none');
 
-    M(nt) = getframe;
+    M(nt) = getframe(f);
     close(f);
 end
 
